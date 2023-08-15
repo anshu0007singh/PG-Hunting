6456ed4372f94fa9876ab69a8ddf707ed07cc207
singh.anshu.rajdev <anshu0007singh@gmail.com>
2023-08-15 17:47:52 +0530

    PG_Property module is completed

--- src/main/java/com/PgHunting/Controller/PropertyController.java
+++ src/main/java/com/PgHunting/Controller/PropertyController.java
@@ -3,6 +3,7 @@ package com.PgHunting.Controller;
 import com.PgHunting.Service.PropertyService;
 import com.PgHunting.util.PropertyRequestDto;
 import com.PgHunting.util.PropertyResponseDto;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,7 +19,7 @@ public class PropertyController {
 
     @PostMapping("/register/{id}")
     public ResponseEntity<PropertyResponseDto> registerProperty(@PathVariable("id") long ownerId
-            ,@RequestBody PropertyRequestDto propertyRequestDto){
+            ,@Valid @RequestBody PropertyRequestDto propertyRequestDto){
         return new ResponseEntity<>(propertyService.registerProperty(ownerId,propertyRequestDto), HttpStatus.CREATED);
     }
 
@@ -29,7 +30,7 @@ public class PropertyController {
 
     @PutMapping("updatePropertyByPropertyId/{id}")
     public ResponseEntity<PropertyResponseDto> updatePropertyByPropertyId(@PathVariable("id") long propertyId
-            ,@RequestBody PropertyRequestDto propertyRequestDto){
+            ,@Valid @RequestBody PropertyRequestDto propertyRequestDto){
         return new ResponseEntity<>(propertyService.updatePropertyByPropertyId(propertyId,propertyRequestDto),HttpStatus.OK);
     }
 
